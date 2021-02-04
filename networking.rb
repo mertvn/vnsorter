a51965b2168c9dea02f2747c0c794db4f0fcd86f
mertvn <mertvn@protonmail.com>
2021-02-04 10:00:38 +0300

    can search vns

--- networking.rb
+++ networking.rb
@@ -1,28 +1,40 @@
 require 'json'
 require 'socket'
 
-# j = JSON.parse()
-# puts j
-# hostname = 'localhost'
-# port = 3000
+# j = JSON.parse('{"more":false,"items":[{"title":"Ever17 -The Out of Infinity-","platforms":["win","ios","and","nds","psp","ps2","drc"],"orig_lang":["ja"],"id":17,"released":"2002-08-29","original":null,"languages":["en","es","ja","ru","zh"]}],"num":1}')
+# p j
+
 hostname = 'api.vndb.org'
 port = 19_534
-# login_req = "login #{j}0x04"
-# puts login_req
-release_req = "get vn basic (id = 17)\04"
+END_CHAR = "\04".freeze
+@socket = TCPSocket.open(hostname, port)
+# @last = ''
+
+login_req = 'login { "protocol": 1, "client": "test", "clientver": 0.1 }'
+release_req = 'get release basic (original~"ハロー・レディ！")'
+puts release_req
 
 def read
-  @message = ''
-  @message << @s.getc until @message[-1] == "\x04"
-  @message[-1] = ''
-  @message
+  message = ''
+  message << @socket.getc until message[-1] == "\x04"
+  message[-1] = ''
+  # @last = message
+  message
 end
 
-@s = TCPSocket.open(hostname, port)
-@s.write 'login '
-@s.write '{ "protocol": 1, "client": "test", "clientver": 0.1 }'
-@s.write "\04"
+def send(message)
+  @socket.write message
+  @socket.write END_CHAR
+end
+
+def parse(message)
+  message.sub!(/\w+ /, '')
+  JSON.pretty_generate(JSON.parse(message))
+end
+send login_req
 puts read
-# @s.write release_req
+send release_req
+puts parse read
 # puts read
-@s.close
+
+@socket.close
