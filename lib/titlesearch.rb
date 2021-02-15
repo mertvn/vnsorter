af396492aadce5f32bcec0a2f792b3164f07d4ea
mertvn <mertvn@protonmail.com>
2021-02-15 20:14:58 +0300

    add smart_querying

--- lib/titlesearch.rb
+++ lib/titlesearch.rb
@@ -7,7 +7,13 @@ module TitleSearch
   def title_query(title)
     @releases = []
 
-    title_filter = "((title~\"#{title}\" or original~\"#{title}\")"
+    title_filter = case $CONFIG['smart_querying']
+                   when true
+                     has_japanese = /[\u3040-\u30ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff\uff66-\uff9f]/.match(title)
+                     has_japanese ? "((original~\"#{title}\")" : "((title~\"#{title}\")"
+                   when false
+                     "((title~\"#{title}\" or original~\"#{title}\")"
+                   end
     title_filter[-1] = $CONFIG['languages'].empty? ? '))' : ") and (languages=#{$CONFIG['languages']}))"
     title_options = '{ "results": 25 }'
     title_final = (REQ_RELEASE + title_filter + title_options).encode('UTF-8')
